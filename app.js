e7ad511b9abaa448d7b04cd3538524123543a45d
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2021-10-10 13:42:48 -0400

    add metadata permission checker to production authenticators

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ if(config.nodeEnv === "PRODUCTION") {
     app.use("/apps/data/:app_id", appAccessCheck);
     app.use("/apps/download/:app_id", appAccessCheck);
     app.use("/apps/metrics/:app_id", appAccessCheck);
+    app.use("/apps/metadata/:app_id", appAccessCheck);
 } else {
     // Outside of production, each request is assigned a user
     // variable with access to every group in the system
