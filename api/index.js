1340a61badbad9530995c0a80818ef2dc03988e9
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2021-10-24 09:26:30 -0400

    remove references to old files

--- api/index.js
+++ api/index.js
@@ -1,18 +1,12 @@
 const apps = require("./routes/apps");
 const appAccess = require("./routes/app-access");
 const metadata = require("./routes/metadata");
-const dataset = require("./routes/dataset");
-const groups = require("./routes/groups");
-const entity = require("./routes/entity");
 const swagger = require("./routes/swagger");
 const tags = require("./routes/tags");
 
 // guaranteed to get dependencies
 module.exports = (app) => {
     metadata(app);
-    dataset(app);
-    groups(app);
-    entity(app);
     swagger(app);
     tags(app);
     apps(app);
